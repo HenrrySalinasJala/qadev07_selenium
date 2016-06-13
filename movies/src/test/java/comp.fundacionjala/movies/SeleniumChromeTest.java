c268ed891d288a2e6deeb66767fcad8dc3ad4bd9
Henrry Salinas <es.henrry@gmail.com>
2016-06-13 16:26:53 -0400

    test by id

--- movies/src/test/java/comp.fundacionjala/movies/SeleniumChromeTest.java
+++ movies/src/test/java/comp.fundacionjala/movies/SeleniumChromeTest.java
@@ -1,8 +1,7 @@
 package comp.fundacionjala.movies;
 
-import static org.junit.Assert.assertTrue;
-
 import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,35 +10,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class SeleniumChromeTest {
-    static final String chromePath="D:/lib/";
+
+    static final String chromePath = "D:/lib/";
     private static WebDriver driver;
+    WebElement element;
 
     @BeforeClass
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.driver", chromePath+"chromedriver.exe");
-        driver=new ChromeDriver();
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", chromePath + "chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(60L, TimeUnit.SECONDS);
-        driver.navigate().to("http://localhost:3000");
+        //driver.navigate().to("http://localhost:3000");
     }
     @Test
-    public void startChromeWebDriver(){
-
-        assertTrue("the title should be mean",driver.getTitle().startsWith("mean"));
+    public void startChromeWebDriver() {
+        driver.get("http://localhost:3000");
+        assertTrue("the title should be mean", driver.getTitle().startsWith("mean"));
         //driver.close();
 
     }
     @Test
-    public void clickLoginButton(){
-        //driver.get("");
-        WebElement loginLnk=driver.findElement(By.id("dropdownMenuLogin"));
+    public void clickLoginButton() {
+        driver.get("http://localhost:3000/#/home");
+        WebElement loginLnk = driver.findElement(By.id("dropdownMenuLogin"));
         loginLnk.click();
+        try {
+            element = driver.findElement(By.name("submit"));
+        } catch (Exception e) {
+        }
+        assertNotNull(element);
     }
     @AfterClass
     public static void tearDown() {
-        if(driver!=null) {
+        if (driver != null) {
             driver.quit();
         }
     }
